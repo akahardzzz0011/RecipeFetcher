e4dea19baaf5a96500b1b7dbe772a34596a0d0c6
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2022-12-04 22:02:53 +0000

    Implemented main screen and fetch recipe layouts

---
+++ app/src/main/java/com/example/recipefetcher/FetchRecipes.java
@@ -0,0 +1,14 @@
+package com.example.recipefetcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FetchRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch_recipes);
+    }
+}
