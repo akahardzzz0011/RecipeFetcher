bbd35f976c536483efb52c6fabf94ddc94e8f345
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2022-12-05 19:43:25 +0000

    Init .jar library

--- app/src/main/java/com/example/recipefetcher/FetchRecipes.java
+++ app/src/main/java/com/example/recipefetcher/FetchRecipes.java
@@ -6,7 +6,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.widget.Button;
-
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.*;
 
 public class FetchRecipes extends AppCompatActivity {
     Button buttonFetchRecipes;
@@ -15,6 +16,7 @@ public class FetchRecipes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch_recipes);
 
+        WebClient webClient = new WebClient();
         buttonFetchRecipes = (Button) findViewById(R.id.buttonFetchRecipes);
 
         buttonFetchRecipes.setOnClickListener(new OnClickListener() {
