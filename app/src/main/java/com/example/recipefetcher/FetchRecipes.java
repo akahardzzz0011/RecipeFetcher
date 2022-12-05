bf1ef53cd0cd719bd7f40f304f1535ee84abf0d1
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2022-12-05 18:09:13 +0000

    Added button

--- app/src/main/java/com/example/recipefetcher/FetchRecipes.java
+++ app/src/main/java/com/example/recipefetcher/FetchRecipes.java
@@ -2,13 +2,26 @@ package com.example.recipefetcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class FetchRecipes extends AppCompatActivity {
-
+    Button buttonFetchRecipes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fetch_recipes);
+
+        buttonFetchRecipes = (Button) findViewById(R.id.buttonFetchRecipes);
+
+        buttonFetchRecipes.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
